51723d60e35195e8768858fcdbea2be013289693
lubremets <liubomyravramets@Liubomyrs-MacBook-Pro.local>
2020-03-10 17:13:33 +0200

    sol

---
+++ build/precache-manifest.716cc3c15fda3191aa2a19a8e0846361.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce0c744bc503df52b66c160277fef93d",
+    "url": "/react_dynamic-list-of-goods/index.html"
+  },
+  {
+    "revision": "b24d937cb58e2b21b029",
+    "url": "/react_dynamic-list-of-goods/static/css/main.21696689.chunk.css"
+  },
+  {
+    "revision": "8f4dba50b955f6cdc04e",
+    "url": "/react_dynamic-list-of-goods/static/js/2.c0f4f830.chunk.js"
+  },
+  {
+    "revision": "5002026598ed5d61cd3d68aaaa6f8e02",
+    "url": "/react_dynamic-list-of-goods/static/js/2.c0f4f830.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b24d937cb58e2b21b029",
+    "url": "/react_dynamic-list-of-goods/static/js/main.b511851e.chunk.js"
+  },
+  {
+    "revision": "84feac7ff8595470aa6a",
+    "url": "/react_dynamic-list-of-goods/static/js/runtime-main.0cf2f8bf.js"
+  },
+  {
+    "revision": "43bfb6f3363d08783d9d1bc1ede7176a",
+    "url": "/react_dynamic-list-of-goods/static/media/background.43bfb6f3.jpg"
+  }
+]);
